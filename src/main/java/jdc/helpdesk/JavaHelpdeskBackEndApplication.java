72558c50ec5e570d22dab30d9f10ecbd00fa1a09
Jorge Do Carmo <jorge.do.carmo123@gmail.com>
2018-04-21 11:39:47 -0300

    Commit Inicial

---
+++ src/main/java/jdc/helpdesk/JavaHelpdeskBackEndApplication.java
@@ -0,0 +1,12 @@
+package jdc.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelpdeskBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelpdeskBackEndApplication.class, args);
+	}
+}

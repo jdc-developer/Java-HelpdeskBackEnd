8a19ce275d8e179910655a513cb234436a2b4c36
Jorge Do Carmo <tidesconfie@gmail.com>
2018-04-23 15:49:02 -0300

    Novos métodos controller de ticket

--- src/main/java/jdc/helpdesk/controller/TicketController.java
+++ src/main/java/jdc/helpdesk/controller/TicketController.java
@@ -1,19 +1,30 @@
 package jdc.helpdesk.controller;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jdc.helpdesk.entity.ChangeStatus;
 import jdc.helpdesk.entity.Ticket;
 import jdc.helpdesk.entity.User;
 import jdc.helpdesk.enums.Status;
@@ -36,6 +47,8 @@ public class TicketController {
 	@Autowired
 	private UserService userService;
 	
+	@PostMapping
+	@PreAuthorize("hasAnyRole('CLIENT')")
 	public ResponseEntity<Response<Ticket>> create(HttpServletRequest request, @RequestBody Ticket ticket,
 			BindingResult result) {
 		Response<Ticket> response = new Response<Ticket>();
@@ -73,5 +86,75 @@ public class TicketController {
 	private Integer generateNumber() {
 		Random random = new Random();
 		return random.nextInt(9999);
+	}
+	
+	@PutMapping
+	@PreAuthorize("hasAnyRole('CLIENT')")
+	public ResponseEntity<Response<Ticket>> update(HttpServletRequest request, @RequestBody Ticket ticket,
+			BindingResult result) {
+		Response<Ticket> response = new Response<Ticket>();
+		try {
+			validateUpdateTicket(ticket, result);
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			Optional<Ticket> ticketCurrent = ticketService.findById(ticket.getId());
+			ticket.setStatus(ticketCurrent.get().getStatus());
+			ticket.setUser(ticketCurrent.get().getUser());
+			ticket.setDate(ticketCurrent.get().getDate());
+			ticket.setNumber(ticketCurrent.get().getNumber());
+			if(ticketCurrent.get().getAssignedUser() != null) {
+				ticket.setAssignedUser(ticketCurrent.get().getAssignedUser());
+			}
+			Ticket ticketPersisted = (Ticket) ticketService.createOrUpdate(ticket);
+			response.setData(ticketPersisted);
+		} catch(Exception e) {
+			return ResponseEntity.badRequest().body(response);
+		}
+		return ResponseEntity.ok(response);
+	}
+	
+	private void validateUpdateTicket(Ticket ticket, BindingResult result) {
+		if(ticket.getId() == 0) {
+			result.addError(new ObjectError("Ticket", "Id não informado"));
+		}
+		if(ticket.getTitle() == null) {
+			result.addError(new ObjectError("Ticket", "Título obrigatório"));
+		}
+	}
+	
+	@GetMapping(value="{id}")
+	@PreAuthorize("hasAnyRole('CLIENT', 'TECHNICIAN')")
+	public ResponseEntity<Response<Ticket>> findById(@PathVariable("id") int id) {
+		Response<Ticket> response = new Response<Ticket>();
+		Optional<Ticket> ticket = ticketService.findById(id);
+		if(ticket == null) {
+			response.getErrors().add("Não encontrado");
+			return ResponseEntity.badRequest().body(response);
+		}
+		List<ChangeStatus> changes = new ArrayList<ChangeStatus>();
+		Iterable<ChangeStatus> changesCurrent = ticketService.listChangeStatus(ticket.get().getId());
+		for(Iterator<ChangeStatus> iterator = changesCurrent.iterator(); iterator.hasNext();) {
+			ChangeStatus changeStatus = (ChangeStatus) iterator.next();
+			changeStatus.setTicket(null);
+			changes.add(changeStatus);
+		}
+		ticket.get().setChanges(changes);
+		response.setData(ticket.get());
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping(value="{id}")
+	@PreAuthorize("hasAnyRole('CLIENT')")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") int id) {
+		Response<String> response = new Response<String>();
+		Optional<Ticket> ticket = ticketService.findById(id);
+		if(ticket == null) {
+			response.getErrors().add("Não encontrado");
+			return ResponseEntity.badRequest().body(response);
+		}
+		ticketService.delete(id);
+		return ResponseEntity.ok(new Response<String>());
 	}
 }

5f9296df60bc3946d222e8b423a289aed7ae1a3e
Jorge Do Carmo <tidesconfie@gmail.com>
2018-04-23 13:07:02 -0300

    Controller de usuário implementada

--- src/main/java/jdc/helpdesk/controller/UserController.java
+++ src/main/java/jdc/helpdesk/controller/UserController.java
@@ -1,15 +1,22 @@
 package jdc.helpdesk.controller;
 
+import java.util.Optional;
+
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +64,70 @@ public class UserController {
 		if(user.getEmail() == null) {
 			result.addError(new ObjectError("User", "Email obrigatório"));
 		}
+	}
+	
+	@PutMapping
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> update(HttpServletRequest request, @RequestBody User user,
+			BindingResult result) {
+		Response<User> response = new Response<User>();
+		try {
+			validateUpdateUser(user, result);
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			user.setPassword(encoder.encode(user.getPassword()));
+			User userPersisted = (User) service.createOrUpdate(user);
+			response.setData(userPersisted);
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		return ResponseEntity.ok(response);
+	}
+	
+	private void validateUpdateUser(User user, BindingResult result) {
+		if(user.getId() == 0) {
+			result.addError(new ObjectError("User", "Id não preenchido"));
+		}
+		if(user.getEmail() == null) {
+			result.addError(new ObjectError("User", "Email obrigatório"));
+		}
+	}
+	
+	@GetMapping(value="{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<User>> findById(@PathVariable("id") int id) {
+		Response<User> response = new Response<User>();
+		Optional<User> user = service.findById(id);
+		if(user == null) {
+			response.getErrors().add("Não encontrado");
+			return ResponseEntity.badRequest().body(response);
+		}
+		response.setData(user.get());
+		return ResponseEntity.ok(response);
+	}
+	
+	@DeleteMapping(value="{id}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<String>> delete(@PathVariable("id") int id) {
+		Response<String> response = new Response<String>();
+		Optional<User> user = service.findById(id);
+		if(user == null) {
+			response.getErrors().add("Não encontrado");
+			return ResponseEntity.badRequest().body(response);
+		}
+		service.delete(id);
+		return ResponseEntity.ok(new Response<String>());
+	}
+	
+	@GetMapping(value="{page}/{count}")
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	public ResponseEntity<Response<Page<User>>> findAll(@PathVariable int page, @PathVariable int count) {
+		Response<Page<User>> response = new Response<Page<User>>();
+		Page<User> users = service.findAll(page, count);
+		response.setData(users);
+		return ResponseEntity.ok(response);
 	}
 }
